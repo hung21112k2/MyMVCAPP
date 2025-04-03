42c57bae880a9841f492928d1eed3a8753fbdf32
hung21112k2 <phamquanghung939@gmail.com>
2025-04-03 19:25:59 +0700

    Mô tả ngắn gọn về thay đổi (ví dụ: 'Update login feature')

---
+++ Views/Products/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyMvcApp.Views.Products
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
